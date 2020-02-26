32db706a1c7da66e77e5754ee5cbb1cf40d0af2a
urage7 <53519075+urage7@users.noreply.github.com>
2020-02-26 14:25:47 +0200

    op

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get(
+      "https://api.unsplash.com/search/photos/?client_id=tPXNEO4kDzT9J19EEK8-v-Bp-pD0KySZM1ZBMR3iCQQ",
+      {
+        params: { query: term },
+        headers: {
+          Authorizatioin:
+            "Client-ID tPXNEO4kDzT9J19EEK8-v-Bp-pD0KySZM1ZBMR3iCQQ"
+        }
+      }
+    );
+    console.log(response.data.results);
   }
 
   render() {
