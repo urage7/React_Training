6a18f0d411321cb2904adc857f7e412cb41105d7
urage7 <53519075+urage7@users.noreply.github.com>
2020-02-26 15:53:10 +0200

    v1 finished !
    1

--- pics/src/components/imageList.js
+++ pics/src/components/imageList.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const ImageList = props => {
-  console.log(props.images);
-  return <div>Image List</div>;
+  const images = props.images.map(image => {
+    return (
+      <img alt={image.description} key={image.id} src={image.urls.regular} />
+    );
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
